8db39059afa846de68260779a02bfe58058b8911
ArbuzKaktus <yurachehet65@gmail.com>
2025-10-23 16:17:26 +0000

    Delete protobuf codegen

--- rules_service/src/lib/rule_factory/rule_factory.hpp
+++ rules_service/src/lib/rule_factory/rule_factory.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <functional>
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <unordered_map>
 
 #include "rule_interface/IRule.hpp"
 #include "threshold_rule/threshold_rule.hpp"
@@ -14,39 +16,46 @@
 class RuleFactory {
 public:
     static RulePtr CreateRuleByType(const rules::RuleConfig& config) {
-        switch (config.rule_type()) {
-            case rules::RuleConfig_RuleType_THRESHOLD:
+        const auto& creators = GetCreators();
+        auto it = creators.find(config.rule_type());
+        
+        if (it == creators.end()) {
+            throw std::runtime_error(
+                "Unknown RuleType: " + std::to_string(config.rule_type()));
+        }
+        
+        return it->second(config);
+    }
+private:
+    using RuleCreator = std::function<RulePtr(const rules::RuleConfig&)>;
+    
+    static const std::unordered_map<rules::RuleConfig_RuleType, RuleCreator>& GetCreators() {
+        static const std::unordered_map<rules::RuleConfig_RuleType, RuleCreator> creators = {
+            {rules::RuleConfig_RuleType_THRESHOLD, [](const rules::RuleConfig& config) -> RulePtr {
                 if (!config.has_threshold_rule()) {
-                    throw std::invalid_argument(
-                        "RuleType is THRESHOLD but threshold_rule not set");
+                    throw std::invalid_argument("RuleType is THRESHOLD but threshold_rule not set");
                 }
                 return std::make_unique<ThresholdRuleAnalyzer>(config);
-
-            case rules::RuleConfig_RuleType_PATTERN:
+            }},
+            {rules::RuleConfig_RuleType_PATTERN, [](const rules::RuleConfig& config) -> RulePtr {
                 if (!config.has_pattern_rule()) {
-                    throw std::invalid_argument(
-                        "RuleType is PATTERN but pattern_rule not set");
+                    throw std::invalid_argument("RuleType is PATTERN but pattern_rule not set");
                 }
                 return std::make_unique<PatternRuleAnalyzer>(config);
-
-            case rules::RuleConfig_RuleType_ML:
+            }},
+            {rules::RuleConfig_RuleType_ML, [](const rules::RuleConfig& config) -> RulePtr {
                 if (!config.has_ml_rule()) {
-                    throw std::invalid_argument(
-                        "RuleType is ML but ml_rule not set");
+                    throw std::invalid_argument("RuleType is ML but ml_rule not set");
                 }
                 return std::make_unique<MlRuleAnalyzer>(config);
-
-            case rules::RuleConfig_RuleType_COMPOSITE:
+            }},
+            {rules::RuleConfig_RuleType_COMPOSITE, [](const rules::RuleConfig& config) -> RulePtr {
                 if (!config.has_composite_rule()) {
-                    throw std::invalid_argument(
-                        "RuleType is COMPOSITE but composite_rule not set");
+                    throw std::invalid_argument("RuleType is COMPOSITE but composite_rule not set");
                 }
                 return std::make_unique<CompositeRuleAnalyzer>(config);
-
-            default:
-                throw std::runtime_error(
-                    "Unknown RuleType: " +
-                    std::to_string(config.rule_type()));
-        }
+            }}
+        };
+        return creators;
     }
 };
